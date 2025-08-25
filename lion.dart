bb6fd09932d87c80b6214b8d949cf781b2943821
Adnan <adn.mayaz@gamil.com>
2025-08-25 01:06:57 +0100

    Lio class created

--- lion.dart
+++ lion.dart
@@ -1,5 +1,22 @@
 import 'zoo.dart';
 
 class Lion extends Animal{
+  late double _maneSize;
+
+  Lion(String name, this._maneSize) : super(name);
+
+  double get maneSize => _maneSize;
+
+  set maneSize(double size){
+    if (size <= 0){
+      throw Exception("Mane size can't be Negative");
+    }
+    _maneSize = size;
+  }
+
+  @override
+  void makeSound() {
+    print("${getName()} roars");
+  }
 
 }
